0b311be7c3976b653a6481d243c7e2116be21be5
ACLECIO PEREIRA DOS SANTOS <aclecio11@gmail.com>
2021-12-30 21:57:32 -0300

    Inicio do curso

---
+++ aula1/aula1.py
@@ -0,0 +1,5 @@
+'''
+094.650.1104-13
+'''
+print('094', '650', '104', sep='.', end='-') # "sep" é um separador e "end" é para não quebrar linha
+print('13')
---
+++ aula2/aula2.py
@@ -0,0 +1,8 @@
+print("alguma coisa")
+print('alguma coisa')
+print(123456)
+print("Isso é uma 'string' (str)")
+print('Isso é uma "string" (str)')
+print("Isso é uma \"string\" (str)")
+print('Isso é uma \'string\' (str)')
+'' "" '() {} []'
